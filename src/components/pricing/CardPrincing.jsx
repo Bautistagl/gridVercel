88b6a4fc3ef90e2eb527e02f5f152a138633fddf
Bautistagl <bautistagonzalezlazo@gmail.com>
2024-06-04 17:20:02 -0300

    dsadas

--- src/components/pricing/CardPrincing.jsx
+++ src/components/pricing/CardPrincing.jsx
@@ -1,24 +1,27 @@
 import React from 'react';
 
-const CardPricing = ({ planType, price, description, features, className }) => {
+const CardPricing = () => {
   return (
-    <div className={`pricing-card ${planType} ${className}`}>
-      <h2>{planType.charAt(0).toUpperCase() + planType.slice(1)}</h2>
-      <h1>
-        ${price}
-        <span>/ per month</span>
-      </h1>
-      <h3>{description}</h3>
-      {features.map((feature, index) => (
-        <p key={index}>{feature}</p>
-      ))}
-      <button
-        className={`button-pricing ${
-          planType === 'team' ? 'button-pricing2' : ''
-        }`}>
-        SELECT
-      </button>
+    <div>
+      <h1>pricing</h1>
     </div>
+    // <div className={`pricing-card ${planType} ${className}`}>
+    //   <h2>{planType.charAt(0).toUpperCase() + planType.slice(1)}</h2>
+    //   <h1>
+    //     ${price}
+    //     <span>/ per month</span>
+    //   </h1>
+    //   <h3>{description}</h3>
+    //   {features.map((feature, index) => (
+    //     <p key={index}>{feature}</p>
+    //   ))}
+    //   <button
+    //     className={`button-pricing ${
+    //       planType === 'team' ? 'button-pricing2' : ''
+    //     }`}>
+    //     SELECT
+    //   </button>
+    // </div>
   );
 };
 
