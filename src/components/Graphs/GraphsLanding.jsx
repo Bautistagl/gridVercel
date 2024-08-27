9f89023959ff77484a9cf6f0fefe927a769f023c
bautista gonzalez lazo <bautistagonzalezlazo@gmail.com>
2024-08-27 12:06:43 -0300

    dsa

--- src/components/Graphs/GraphsLanding.jsx
+++ src/components/Graphs/GraphsLanding.jsx
@@ -5,14 +5,7 @@ const GraphsLanding = () => {
   return (
     <div className="graphs-landing">
       <div style={{ display: "flex" }}>
-        <div>
-          <h3>SOLUTIONS</h3>
-          <h1>A fraction of the cost for real privacy</h1>
-        </div>
-        <span>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
-        </span>
+        <div></div>
       </div>
       {/* <div style={{ display: 'flex', margin: 'auto', width: '80%' }}>
         <Image alt="" src="/costos.svg" height={400} width={600} />
